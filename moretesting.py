c4722a01f13e9cfd20b7e9278f2d7cb667e16ca3
Mathew Langlois <mathew_langlois@student.uml.edu>
2024-12-09 15:25:08 -0500

    Latest audio testing

--- moretesting.py
+++ moretesting.py
@@ -1,7 +1,13 @@
 import pyaudio
 import numpy as np
+from network_model import FullyConnectedNN
+import torch
+import csv
+import generate_training_data as gen
+import preprocess_audio as pre
+from os.path import join
 
-def record_last_second(sample_rate=44100, channels=1, chunk_size=1024):
+def record_last_second(sample_rate=44100, channels=1, chunk_size=512):
     """
     Records the last second of audio and returns it as a NumPy array.
 
@@ -38,6 +44,37 @@ def record_last_second(sample_rate=44100, channels=1, chunk_size=1024):
 
 # Example usage
 if __name__ == "__main__":
+    # Make sure to define the same model architecture
+    # First we need the number of training features for input size (or just hardcode it if you know it)
+    training_dataset = "data_v3.csv"
+    with open(training_dataset, 'r') as file:
+        reader = csv.reader(file)
+        first_line = next(reader)  # Get the first line
+        num_features = len(first_line[1:])
+    print(f"Features found in dataset: {num_features}")
+    audio_chunk = record_last_second()
+    audio = pre.process_non_silent_clip(audio_chunk, 44100, clip_duration=1.0, step_size=1.0)
+    features = gen.generate_features(audio[0], 44100)
+    print(features.shape)
+    model = FullyConnectedNN(features.shape[1])
+    model_dir = "./models"
+    model_name = "model_v1.pth"
+    
+    # Load only the state dictionary safely
+    state_dict = torch.load(join(model_dir,model_name), weights_only=True)
+    # Load it into your model
+    model.load_state_dict(state_dict)
+    # Set the model to evaluation mode
+    model.eval()  
+
+    print("Model loaded successfully!")
+    
     while True:
         audio_chunk = record_last_second()
-        print(f"Recorded audio chunk with shape: {audio_chunk.shape}")
+        audio = pre.process_non_silent_clip(audio_chunk, 44100, clip_duration=1.0, step_size=1.0)
+        features = gen.generate_features(audio[0], 44100)
+        # model(features)
+        preds = torch.argmax(model(features), dim=1)
+        print(preds)
+        # print(f"Recorded audio chunk with shape: {audio_chunk.shape}")
+        
